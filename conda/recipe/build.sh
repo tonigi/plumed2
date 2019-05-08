1048f380311016b4e4b97364e4dc6c5dc0b1e3d1
Toni <toni.giorgino@gmail.com>
2019-05-09 00:09:15 +0200

    nonsense

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -2,29 +2,23 @@
 
 env | sort
 
-if [[ $target_platform == linux* ]]; then
-    condaldflags="$BUILD_PREFIX/lib/libz.a $BUILD_PREFIX/lib/libz.so.1 $BUILD_PREFIX/lib/libxdrfile.a $BUILD_PREFIX/lib/libxdrfile.so"
-elif [[ $target_platform == osx* ]]; then
-    # Possibly not needed.
-    condaldflags="$BUILD_PREFIX/lib/libz.a $BUILD_PREFIX/lib/libz.dylib $BUILD_PREFIX/lib/libxdrfile.a $BUILD_PREFIX/lib/libxdrfile.dylib"
-else
-    echo "Unexpected platform $target_platform"
-fi
-
 
 
 # GB: install xdrfile library
 wget http://ftp.gromacs.org/pub/contrib/xdrfile-1.1.4.tar.gz
 tar xzf xdrfile-1.1.4.tar.gz
-cd xdrfile-1.1.4
-./configure --prefix=$PREFIX --enable-shared
-make
-make install
-cd ../
+(
+    cd xdrfile-1.1.4
+    ./configure --prefix=$PREFIX --enable-shared
+    make
+    make install
+)
 
 
 
 # Thanks to https://github.com/intbio/plumed-conda/blob/master/plumed2_v2.5.0/build.sh
+condaldflags="$BUILD_PREFIX/lib/libz.a $BUILD_PREFIX/lib/libz$SHLIB_EXT
+	      $BUILD_PREFIX/lib/libxdrfile.a $BUILD_PREFIX/lib/libxdrfile$SHLIB_EXT"
 
 ./configure --prefix=$PREFIX --enable-shared --disable-python --disable-external-lapack --disable-external-blas LDFLAGS="$condaldflags"
 make -j4
