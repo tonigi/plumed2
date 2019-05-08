0924a7b9382153c34983a11590a6606e678767d7
Toni <toni.giorgino@gmail.com>
2019-05-08 23:36:49 +0200

    nn

--- conda/run-conda-upload.sh
+++ conda/run-conda-upload.sh
@@ -16,7 +16,7 @@ export PATH="$CONDA_HOME/bin:$PATH"
 
 # Disabled because it fails
 if [[ -n "$CONDA_UPLOAD_TOKEN" ]]; then
-    USER=plumed  # the conda channel
+    USER=tonigi  # the conda channel
     anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l testing \
 	     $CONDA_HOME/conda-bld/$TRAVIS_OS_NAME-64/plumed*.tar.bz2 --force
 fi
