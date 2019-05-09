29c6d20760b711e5eff0926280a65abdbff0c7de
Toni G <tonigi@users.noreply.github.com>
2019-05-09 12:12:08 +0200

    Update build.sh

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -20,7 +20,7 @@ tar xzf xdrfile-1.1.4.tar.gz
 condaldflags="$BUILD_PREFIX/lib/libz.a $BUILD_PREFIX/lib/libz$SHLIB_EXT \
 	      $BUILD_PREFIX/lib/libxdrfile.a $BUILD_PREFIX/lib/libxdrfile$SHLIB_EXT"
 
-LDFLAGS="$condaldflags" ./configure --prefix=$PREFIX --enable-shared --disable-python --disable-external-lapack --disable-external-blas
+./configure --prefix=$PREFIX --enable-shared --disable-python --disable-external-lapack --disable-external-blas LDFLAGS="-L$BUILD_PREFIX/lib"
 make -j4
 make install
 
