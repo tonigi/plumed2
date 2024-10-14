4b81b97660f2e59e22a9c6b6ee072e3c285c7197
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-10-14 15:08:44 +0200

    removed the self-import gor getting the path of pycv

--- plugins/pycv/src/pycv/__init__.py
+++ plugins/pycv/src/pycv/__init__.py
@@ -2,10 +2,15 @@
 
 
 def getPythonCVInterface():
-    import inspect
-    import pycv
+    """returns the location of the pycv shared object"""
+    # older version
+    # import inspect
+    # import pycv
 
-    path_of_this = inspect.getfile(pycv)
+    # path_of_this = inspect.getfile(pycv)
+    import importlib.util
+
+    path_of_this = importlib.util.find_spec("pycv").origin
     path_of_lib = path_of_this[: path_of_this.rfind("/")]
     return path_of_lib + "/PythonCVInterface.so"
 
