a4875958ea8c16574880cccc32a405b72da26ef1
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-10-14 14:16:30 +0200

    setting up a second run option, adding an helper

--- plugins/pycv/src/pycv/__init__.py
+++ plugins/pycv/src/pycv/__init__.py
@@ -1,8 +1,8 @@
-import inspect
 # checked and formatted with ruff 0.6.9
 
 
 def getPythonCVInterface():
+    import inspect
     import pycv
 
     path_of_this = inspect.getfile(pycv)
@@ -11,5 +11,15 @@ def getPythonCVInterface():
 
 
 def main():
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        prog="pyCV",
+        description="""shows the path for the pycv shared object.
+
+        Just run this with no arguments to see the path.
+        """,
+    )
+    _ = parser.parse_args()
     print(getPythonCVInterface())
     return 0
